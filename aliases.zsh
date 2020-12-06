89dc5406780b7f0c3a203a853169dbebed36b115
jprtal <j.portal@outlook.com>
2020-12-06 21:22:05 +0100

    Update Zsh

--- aliases.zsh
+++ aliases.zsh
@@ -10,3 +10,5 @@ alias mydns="nmcli dev show | grep DNS"
 alias mitm="docker run --rm -it -v $HOME/.mitmproxy:/home/mitmproxy/.mitmproxy -p 8080:8080 mitmproxy/mitmproxy"
 alias mitm-web="docker run --rm -it -v $HOME/.mitmproxy:/home/mitmproxy/.mitmproxy -p 8080:8080 -p 127.0.0.1:8081:8081 mitmproxy/mitmproxy mitmweb --web-host 0.0.0.0"
 alias aulavirtual="python /opt/aula-virtual-dl/aula_virtual.py -r $HOME/Documents/aula_virtual -w 3 -c 1Q -u"
+
+alias sound-cards="cat /proc/asound/cards | grep ' -'"
--- functions.zsh
+++ functions.zsh
@@ -1,3 +1,7 @@
 function cert-sha1() {
     openssl x509 -inform PEM -pubkey -noout -in $1 | openssl pkey -pubin -outform DER | openssl dgst -sha1 -binary | openssl enc -base64
 }
+
+function sound-card-params() {
+    cat /proc/asound/card$1/pcm0p/sub0/hw_params
+}
